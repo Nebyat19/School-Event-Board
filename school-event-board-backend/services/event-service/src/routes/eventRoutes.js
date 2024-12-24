5ad8c0e00fc4fb5ceccc56fd17dcdeba5f7db90a
Nebyat B <xhetu.dev@gmail.com>
2024-12-24 23:07:43 +0300

    chore: add initial type definitions and configuration files for various modules

--- school-event-board-backend/services/event-service/src/routes/eventRoutes.js
+++ school-event-board-backend/services/event-service/src/routes/eventRoutes.js
@@ -4,5 +4,9 @@ const { createEvent } = require('../controllers/eventController');
 const router = express.Router();
 
 router.post('/', createEvent);
+router.get('/', (req, res) => {
+    res.send('Event Service is running');
+}
+)
 
 module.exports = router;
